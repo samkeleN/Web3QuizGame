d02f81d65b69a9fdefb5761cb58875ac32ac223d
Oshadhi Liyanage <oshadhi@surge.global>
2025-05-07 09:03:01 +0530

    chore: remove unused font

--- proof-of-ship/src/app/layout.tsx
+++ proof-of-ship/src/app/layout.tsx
@@ -3,17 +3,11 @@ import type { Metadata } from "next";
 import { getSession } from "~/auth";
 import "~/app/globals.css";
 import { Providers } from "~/app/providers";
-import { Londrina_Solid } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Proof of Ship",
   description: "Proof of Ship Frame",
 };
-export const londrina = Londrina_Solid({
-  weight: "400",
-  subsets: ["latin"],
-  display: "swap",
-});
 
 export default async function RootLayout({
   children,
